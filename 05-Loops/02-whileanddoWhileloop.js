d198e39f155151e27a48910d7db78bcaec3d27bf
Tarun Dhouni <tarundhouni2000@gmail.com>
2025-05-26 15:47:10 +0530

    While and DO- While Loop

---
+++ 05-Loops/02-whileanddoWhileloop.js
@@ -0,0 +1,26 @@
+//While loop
+//Syntax
+/*while(condition){
+
+
+increment;
+}*/ 
+
+let i =1; // if we make it 0 it will become 
+
+while(i<=20){
+    console.log(` The value is ${i}`)
+    i=i*2 //icrement the value by mutliplying it 
+}
+
+// Do while Loop (first execute then apply condition)
+
+let j = 1;
+
+do{
+    console.log("Enter the value " ,j);
+    j =j*2
+
+}while(j<=50);
+
+// Its Like pehle Kam karo phir condition check karo 
